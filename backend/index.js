b8210e81ee7e57232bc69aa964c96a4c0221ba83
unknown <newlykung@gmail.com>
2025-05-15 15:43:10 +0700

    Add user registration and password hashing

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,52 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 const PORT = 3001;
 const JWT_SECRET = "1234567890";
+const salt = 10;
 
 app.use(cors());
 app.use(express.json());
 
+app.post("/register", async (req, res) => {
+    const {register_username, register_password} = req.body;
+
+    console.log("register_username = ", register_username);
+    console.log("register_password = ", register_password);
+
+    const user = await db("user").where({user_username: register_username}).first();
+
+    if (user) {
+        return res.status(400).json("User already exists");
+    }
+
+    try {
+        const hash = await bcrypt.hash(register_password, salt)
+        
+        console.log("Hash password = ",hash)
+
+        const register = await db("user").insert({
+            user_username: register_username,
+            user_password: hash
+        })
+        res.json({
+            message: "Register success",
+            data: register
+        })
+    } catch (error) {
+        console.error("Error inserting user: ", error);
+        return res.status(500).json("Error inserting user");
+    }
+})
+
 app.post("/login", async (req, res) => {
     console.log("req.body = ", req.body);
-    const {username, password} = req.body;
+    const username = req.body.username;
+    const password = req.body.password;
 
     console.log("username = ", username);
     console.log("password = ", password);
@@ -22,8 +56,15 @@ app.post("/login", async (req, res) => {
 
     console.log("user = ", user);
 
-    if (!user.user_username && !user.user_password) {
-        return res.status(401).json("Invalid username or password");
+    if (!user) {
+        return res.status(401).json("User not found");
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.user_password)
+    console.log("checkPassword = ", checkPassword);
+
+    if (!checkPassword) {
+        return res.status(401).json("Incorrect password")
     }
 
     const token = jwt.sign({id: user.user_id, username: user.user_username}, JWT_SECRET, {expiresIn: "1h"});
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,6 +5,8 @@ if (document.getElementById("loginForm")) {
             const username = document.getElementById("username").value;
             const password = document.getElementById("password").value;
 
+            console.log("data = ", password)
+
             const response = await fetch("http://localhost:3001/login", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
@@ -21,6 +23,33 @@ if (document.getElementById("loginForm")) {
                 alert("Invalid username or password");
             }
         });
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+if (document.getElementById("registerForm")) {
+    try {
+        document.getElementById("registerForm").addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const register_username = document.getElementById("register_username").value;
+            const register_password = document.getElementById("register_password").value;
+            const register_password_confirm = document.getElementById("register_password_confirm").value;
+
+            if (register_password !== register_password_confirm) {
+                alert("Passwords do not match");
+                return;
+            }
+
+            const response = await fetch("http://localhost:3001/register", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({register_username, register_password})
+            })
+
+            const data = await response.json();
+            console.log("response = ", data);
+        })
     } catch (error) {
         console.error("Error:", error);
     }
